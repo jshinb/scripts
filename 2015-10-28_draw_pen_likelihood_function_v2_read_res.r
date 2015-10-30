6ebd0388d0d0ced8075cf6eee716754be5156d41
[jean shin] <[shin.jihyung@gmail.com]>
2015-10-30 15:21:40 -0400

    modified

--- 2015-10-28_draw_pen_likelihood_function_v2_read_res.r
+++ 2015-10-28_draw_pen_likelihood_function_v2_read_res.r
@@ -18,7 +18,9 @@ par(mfrow=c(1,2),ask=T)
 for(rep.id in 1:length(cont.tables)){
   mytitle = paste("irep-",rep.id,"; ", "Rao Score test P-value = ",
                   round(anova(glm0.list[[rep.id]], glm1.list[[rep.id]], test="Rao")$P[2],4),sep="")
-
+  mytitlep = paste("irep-",rep.id,"; ", "Penalized Score test P-value = ",
+                   round(plm.score[rep.id],4),sep="")
+  
   plot(mybeta1s,test.log0.mat[,rep.id],type="l",ylab="log-likelihood",main=mytitle)
   lines(mybeta1s,test.log1.mat[,rep.id],lty=3,col="red");
   abline(v=glm1.list[[rep.id]]$coef["z.x1TRUE"],h=logLik(glm1.list[[rep.id]]),col="pink",lty=2)
